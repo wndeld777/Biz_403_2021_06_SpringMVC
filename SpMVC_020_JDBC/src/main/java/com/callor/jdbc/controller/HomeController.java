7b8afc8835b346e73561194871bbcb25ce9b24c2
wndeld777 <wndeld777@gmail.com>
2021-06-08 16:26:51 +0900

    css

--- SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
+++ SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
@@ -1,37 +1,36 @@
 package com.callor.jdbc.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.callor.jdbc.pesistance.BookDao;
+import com.callor.jdbc.service.RentService;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
+	/*
+	 * 보편적인 Spring 에서 bean을 사용하는 코드
+	 * @Autowired
+	 * private BookDao bookdao
+	 * 이 코드에서 메모리 leak(누수)현상이 보고되어
+	 * 다음의 코드를 권장한다.
 	 */
+	// protected final BookDao bookDao;
+	protected final RentService rentService;
+	public HomeController(RentService rentService) {
+		this.rentService = rentService;
+	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		rentService.viewBookAndComp();
 		
 		return "home";
 	}
