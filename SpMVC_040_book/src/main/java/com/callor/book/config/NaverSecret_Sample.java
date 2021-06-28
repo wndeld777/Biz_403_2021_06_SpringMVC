5575276c614986de362d57ae37121aeaa958a6c6
wndeld777 <wndeld777@gmail.com>
2021-06-28 15:17:44 +0900

    NaverSearch

---
+++ SpMVC_040_book/src/main/java/com/callor/book/config/NaverSecret_Sample.java
@@ -0,0 +1,14 @@
+package com.callor.book.config;
+
+public class NaverSecret_Sample {
+
+	/*
+	 * 네이버 개발자센터에 어플을 등록한 후
+	 * CLIENT_ID, CLIENT_SECRET을 발급받아
+	 * 다음 항목에 작성한 후
+	 * 파일 이름을 NaverSecret로 변경한 후
+	 * 프로젝트를 실행하시오
+	 */
+	public static final String NAVER_CLIENT_ID = "YOUR NAVER CLINET ID";
+	public static final String NAVER_CLIENT_SECRET = "YOUT NAVER SECRET";
+}
